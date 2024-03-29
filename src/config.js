a473574b9baf3c905352f3114385fabf8eb8bd57
hameed Shaik <hameedsk381@gmail.com>
2024-03-29 15:18:30 +0530

    added env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
-const REACT_API_URL = process.env.SERVER_URL || "https://christtheking-l964.onrender.com"
+const REACT_API_URL = process.env.SERVER_URL || process.env.VITE_SERVER_URL
 // const REACT_API_URL ="http://localhost:5000"
  export default REACT_API_URL
